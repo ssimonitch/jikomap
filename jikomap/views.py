aa296f159ec0e365a7cae80a69836520115702db
goldenyusuke <nshinichiwork001@gmail.com>
2019-09-01 17:07:35 +0900

    create views, index, urls

--- jikomap/views.py
+++ jikomap/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('jikomap/index.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
 
 # Create your views here.

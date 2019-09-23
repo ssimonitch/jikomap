fb83cf97d280870b604bcc9255727f84a4afa573
goldenyusuke <nshinichiwork001@gmail.com>
2019-09-23 11:40:11 +0900

    DATABASES

--- config/settings.py
+++ config/settings.py
@@ -156,4 +156,4 @@ if not DEBUG:
     django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'.update(db_from_env)]
+DATABASES['default'].update(db_from_env)
